104525e759eeb00dfe8f6a678cc6e86c48d57748
Amaury <amaurymechin37@gmail.com>
2023-12-11 15:07:16 +0100

    Auth login/register

---
+++ lib/auth/login_page.dart
@@ -0,0 +1,127 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'signin_page.dart';
+
+// Classe représentant la page de connexion
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+// Classe d'état pour la page de connexion
+class _LoginPageState extends State<LoginPage> {
+  // Contrôleurs pour gérer la saisie de l'utilisateur dans les champs d'e-mail et de mot de passe
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  bool _obscurePassword = true;
+
+  // Méthode de connexion de l'utilisateur
+  void _login() async {
+    try {
+      // Tentative de connexion de l'utilisateur avec l'e-mail et le mot de passe fournis
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      );
+
+      // Affichage d'un message de réussite à l'aide d'un Snackbar
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Connexion réussie!'),
+          duration: Duration(seconds: 3),
+        ),
+      );
+
+      // Navigation vers la page suivante après une connexion réussie
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          // builder: (context) => LoggedPage(
+          //   userEmail: userCredential.user?.email ?? '',
+          // ),
+          builder: (context) => LoginPage(),
+        ),
+      );
+    } on FirebaseAuthException catch (e) {
+      // Gestion des exceptions FirebaseAuth, telles qu'un e-mail ou un mot de passe incorrect
+      print("Erreur lors de la connexion: $e");
+
+      // Affichage d'un message d'erreur à l'aide d'un Snackbar
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Erreur lors de la connexion'),
+          duration: Duration(seconds: 3),
+        ),
+      );
+    }
+  }
+
+  // Méthode pour accéder à la page d'inscription
+  void _goToRegistration() {
+    // Redirection vers la page d'inscription
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(
+        builder: (context) => AuthPage(),
+      ),
+    );
+  }
+
+  // Méthode de construction de l'interface utilisateur de la page
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // Barre d'app avec le titre "Connexion"
+      appBar: AppBar(
+        title: const Text('Connexion'),
+      ),
+      // Corps de la page avec des champs de saisie d'e-mail et de mot de passe, et des boutons d'action
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Champ de texte pour saisir l'e-mail
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(labelText: 'Email'),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            const SizedBox(height: 16.0), // Espacement vertical
+            // Champ de texte pour saisir le mot de passe
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                labelText: 'Mot de passe',
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    // Inverse l'état d'obscuration du mot de passe lors du clic sur le bouton
+                    setState(() {
+                      _obscurePassword = !_obscurePassword;
+                    });
+                  },
+                  icon: Icon(
+                    // Utilise une icône différente selon l'état d'obscuration
+                    _obscurePassword ? Icons.visibility : Icons.visibility_off,
+                  ),
+                ),
+              ),
+              obscureText:
+                  _obscurePassword, // Mot de passe masqué en fonction de l'état
+            ),
+            const SizedBox(height: 32.0), // Espacement vertical
+            // Bouton élevé pour déclencher la connexion
+            ElevatedButton(
+              onPressed: _login,
+              child: const Text('Se connecter'),
+            ),
+            const SizedBox(height: 16.0), // Espacement vertical
+            // Bouton de texte pour accéder à la page d'inscription
+            TextButton(
+              onPressed: _goToRegistration,
+              child: const Text('Pas de compte ? S\'inscrire'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/auth/signin_page.dart
@@ -0,0 +1,183 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+
+class AuthPage extends StatefulWidget {
+  @override
+  _AuthPageState createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  // Contrôleurs pour gérer la saisie de l'utilisateur dans les champs d'e-mail et de mot de passe
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  bool _obscurePassword = true;
+
+  // Vérifier si le mot de passe contient au moins une lettre majuscule
+  bool _containsUppercase(String password) {
+    return password.contains(RegExp(r'[A-Z]'));
+  }
+
+  // Vérifier si le mot de passe contient au moins un caractère spécial
+  bool _containsSpecialCharacter(String password) {
+    return password.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'));
+  }
+
+  bool _containsNumber(String value) {
+    // Vérifie si la chaîne contient au moins un chiffre
+    return value.contains(RegExp(r'\d'));
+  }
+
+  // Méthode d'enregistrement de l'utilisateur
+  void _register() async {
+    try {
+      // Vérifier si l'e-mail est vide
+      if (_emailController.text.isEmpty) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Veuillez saisir une adresse e-mail.'),
+            duration: Duration(seconds: 3),
+          ),
+        );
+        return;
+      }
+      // Vérifier si le mot de passe est vide
+      if (_passwordController.text.isEmpty) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Veuillez saisir un mot de passe.'),
+            duration: Duration(seconds: 3),
+          ),
+        );
+        return;
+      }
+
+      // Vérifier si le mot de passe répond aux critères
+      if (_passwordController.text.length < 8 ||
+          !_containsUppercase(_passwordController.text) ||
+          !_containsSpecialCharacter(_passwordController.text) ||
+          !_containsNumber(_passwordController.text)) {
+        // Afficher un message d'erreur si le mot de passe est invalide
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text(
+                'Mot de passe invalide. Assurez-vous qu\'il a au moins 8 caractères, une majuscule et un caractère spécial.'),
+            duration: Duration(seconds: 5),
+          ),
+        );
+        return;
+      }
+      // Tentative de création d'un nouvel utilisateur avec l'e-mail et le mot de passe fournis
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      );
+
+      // Affichage d'un message de réussite à l'aide d'un Snackbar
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Inscription réussie !'),
+          duration: Duration(seconds: 3),
+        ),
+      );
+
+      // Navigation vers la page de connexion après une inscription réussie
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => LoginPage(),
+        ),
+      );
+    } on FirebaseAuthException catch (e) {
+      // Gestion des exceptions FirebaseAuth
+      if (e.code == 'email-already-in-use') {
+        // L'e-mail est déjà associé à un compte existant
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Cet e-mail est déjà associé à un compte existant.'),
+            duration: Duration(seconds: 3),
+          ),
+        );
+      } else {
+        // Autre erreur lors de l'inscription
+        print("Erreur lors de l'inscription : $e");
+
+        // Affichage d'un message d'erreur à l'aide d'un Snackbar
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Erreur lors de l\'inscription'),
+            duration: Duration(seconds: 3),
+          ),
+        );
+      }
+    }
+  }
+
+  // Méthode pour accéder à la page de connexion
+  void _goToLoginPage() {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(
+        builder: (context) => LoginPage(),
+      ),
+    );
+  }
+
+  // Méthode build pour créer l'interface utilisateur de la page d'authentification
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // Barre d'app avec le titre "Inscription"
+      appBar: AppBar(
+        title: const Text('Inscription'),
+      ),
+      // Corps de la page contenant les champs de saisie de l'utilisateur et les boutons
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Champ de texte pour saisir l'e-mail
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(labelText: 'E-mail'),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            const SizedBox(height: 16.0),
+            // Champ de texte pour saisir le mot de passe
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                labelText: 'Mot de passe',
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    // Inverse l'état d'obscuration du mot de passe lors du clic sur le bouton
+                    setState(() {
+                      _obscurePassword = !_obscurePassword;
+                    });
+                  },
+                  icon: Icon(
+                    // Utilise une icône différente selon l'état d'obscuration
+                    _obscurePassword ? Icons.visibility : Icons.visibility_off,
+                  ),
+                ),
+              ),
+              obscureText:
+                  _obscurePassword, // Mot de passe masqué en fonction de l'état
+            ),
+            const SizedBox(height: 32.0),
+            // Bouton élevé pour déclencher l'enregistrement
+            ElevatedButton(
+              onPressed: _register,
+              child: const Text("S'inscrire"),
+            ),
+            const SizedBox(height: 16.0),
+            // Bouton de texte pour accéder à la page de connexion
+            TextButton(
+              onPressed: _goToLoginPage,
+              child: const Text('Déjà un compte ? Se connecter'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
