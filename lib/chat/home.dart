83e86947073cf129fb9c935cb95b78fd75ddb2a5
Hippot <hippot02@gmail.com>
2024-01-02 12:39:20 +0100

    ajout commentaires

--- lib/chat/home.dart
+++ lib/chat/home.dart
@@ -1,14 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:intl/intl.dart';
+import 'package:chatsuble/profile/profile_page.dart';
 import 'package:chatsuble/chat/widgets/_list_tile.dart';
 import 'package:chatsuble/chat/widgets/_stateful_dialog_button.dart';
 import 'package:chatsuble/chat/widgets/distance_calculator.dart';
 import 'package:chatsuble/chat/widgets/filter/_filter_dialog.dart';
 import 'package:chatsuble/chat/widgets/filter/filter_button.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import 'package:chatsuble/profile/profile_page.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -118,6 +117,7 @@ class HomePageContent extends StatelessWidget {
                           theme: messageTheme,
                           text: messageText,
                           date: formattedDate,
+                          messageId: data['messageId'],
                         );
                       },
                     );
@@ -139,7 +139,10 @@ class HomePageContent extends StatelessWidget {
 
       final QuerySnapshot snapshot = await messagesCollection.get();
       List<Map<String, dynamic>> messages = snapshot.docs
-          .map((doc) => doc.data() as Map<String, dynamic>)
+          .map((doc) => {
+                ...doc.data() as Map<String, dynamic>,
+                'messageId': doc.id,
+              })
           .toList();
 
       messages.sort((a, b) {
@@ -150,8 +153,6 @@ class HomePageContent extends StatelessWidget {
       });
 
       Position userLocation = await _getUserLocation();
-      double userDistance = await _getUserDistance();
-      print(userDistance);
 
       messages = messages.where((message) {
         if (message.containsKey('latitude') &&
@@ -198,32 +199,5 @@ class HomePageContent extends StatelessWidget {
         headingAccuracy: 0,
       );
     }
-  }
-
-  Future<double> _getUserDistance() async {
-    // Récupérez l'utilisateur actuellement connecté
-    User? user = FirebaseAuth.instance.currentUser;
-
-    if (user != null) {
-      // Obtenez l'ID de l'utilisateur connecté
-      String userId = user.uid;
-
-      // Utilisez l'ID de l'utilisateur pour lire la distance du document utilisateur
-      DocumentSnapshot userSnapshot = await FirebaseFirestore.instance
-          .collection('users')
-          .doc(userId)
-          .get();
-
-      if (userSnapshot.exists) {
-        // Accédez au champ "distance" du document utilisateur et renvoyez la valeur
-        dynamic distance = userSnapshot['distance'];
-        return distance.toDouble();
-      } else {
-        print('L\'utilisateur avec l\'ID $userId n\'existe pas.');
-        return 0.0; // Ou une valeur par défaut
-      }
-    }
-
-    return 0.0; // Ou une valeur par défaut
   }
 }
--- lib/chat/widgets/_list_tile.dart
+++ lib/chat/widgets/_list_tile.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class MyListTile extends StatelessWidget {
   final String theme;
   final String text;
   final String date;
+  final String messageId;
 
   const MyListTile({
     Key? key,
     required this.theme,
     required this.text,
     required this.date,
+    required this.messageId,
   }) : super(key: key);
 
   Color getThemeColor(String theme) {
@@ -46,8 +50,7 @@ class MyListTile extends StatelessWidget {
             RichText(
               text: TextSpan(
                 style: DefaultTextStyle.of(context).style.copyWith(
-                      fontSize:
-                          15, // Ajustez la taille du titre selon vos préférences
+                      fontSize: 15,
                     ),
                 children: [
                   TextSpan(
@@ -65,7 +68,131 @@ class MyListTile extends StatelessWidget {
         ),
         subtitle: Text('Envoyé le $date',
             style: const TextStyle(color: Colors.white)),
+        trailing: IconButton(
+          icon: Icon(Icons.comment),
+          onPressed: () {
+            _showCommentsDialog(context, messageId);
+          },
+        ),
       ),
     );
+  }
+
+  void _showCommentsDialog(BuildContext context, String messageId) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return CommentsDialog(messageId: messageId);
+      },
+    );
+  }
+}
+
+class CommentsDialog extends StatefulWidget {
+  final String messageId;
+
+  CommentsDialog({required this.messageId});
+
+  @override
+  _CommentsDialogState createState() => _CommentsDialogState();
+}
+
+class _CommentsDialogState extends State<CommentsDialog> {
+  TextEditingController _commentController = TextEditingController();
+  List<String> _comments = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadComments();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: _comments.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                      title: Text(_comments[index]),
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(
+                height:
+                    16), // Ajouter un espace entre la liste de commentaires et le champ de texte
+            TextField(
+              controller: _commentController,
+              decoration: InputDecoration(labelText: 'Ajouter un commentaire'),
+            ),
+            SizedBox(
+                height:
+                    16), // Ajouter un espace entre le champ de texte et les boutons
+            Row(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    _addComment(widget.messageId, _commentController.text);
+                    _commentController
+                        .clear(); // Effacer le champ de commentaire après l'ajout
+                    _loadComments(); // Rafraîchir les commentaires après ajout
+                  },
+                  child: Text('Ajouter'),
+                ),
+                SizedBox(width: 16), // Ajouter un espace entre les boutons
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context)
+                        .pop(); // Fermer la fenêtre des commentaires
+                  },
+                  child: Text('Fermer'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _loadComments() async {
+    try {
+      final DocumentSnapshot snapshot = await FirebaseFirestore.instance
+          .collection('messages')
+          .doc(widget.messageId)
+          .get();
+
+      final List<dynamic> comments = snapshot['comments'] ?? [];
+      setState(() {
+        _comments = comments.cast<String>().toList();
+      });
+    } catch (e) {
+      print('Erreur lors du chargement des commentaires: $e');
+      setState(() {
+        _comments = [];
+      });
+    }
+  }
+
+  void _addComment(String messageId, String comment) async {
+    try {
+      await FirebaseFirestore.instance
+          .collection('messages')
+          .doc(messageId)
+          .update({
+        'comments': FieldValue.arrayUnion([comment]),
+      });
+    } catch (e) {
+      print('Erreur lors de l\'ajout du commentaire: $e');
+    }
   }
 }
